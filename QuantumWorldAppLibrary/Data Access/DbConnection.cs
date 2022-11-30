1498fef8ca18b6fbd71206b7545fa1a1f5c07bbd
Kamil Preweda <preweda.kamil@o2.pl>
2022-11-30 15:54:56 +0100

    Created Database Connection Class

--- QuantumWorldAppLibrary/Data Access/DbConnection.cs
+++ QuantumWorldAppLibrary/Data Access/DbConnection.cs
@@ -1,5 +1,30 @@
-﻿
+﻿using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
 namespace QuantumWorldAppLibrary.Data_Access;
-public class DbConnection
+public class DbConnection : IDbConnection
 {
+   private readonly IConfiguration _config;
+   private readonly IMongoDatabase _db;
+   private string _connectionId = "MongoDB";
+   public string DbName { get; private set; }
+   public string BuildingCollectionName { get; private set; } = "buildings";
+   public string ResourceCollectionName { get; private set; } = "resources";
+   public string UserCollectionName { get; private set; } = "users";
+   public MongoClient Client { get; private set; }
+   public IMongoCollection<BuildingModel> BuildingCollection { get; private set; }
+   public IMongoCollection<ResourceModel> ResourceCollection { get; private set; }
+   public IMongoCollection<UserModel> UserCollection { get; private set; }
+
+   public DbConnection(IConfiguration config)
+   {
+      _config = config;
+      Client = new MongoClient(_config.GetConnectionString(_connectionId));
+      DbName = _config["DatabaseName"];
+      _db = Client.GetDatabase(DbName);
+
+      BuildingCollection = _db.GetCollection<BuildingModel>(BuildingCollectionName);
+      ResourceCollection = _db.GetCollection<ResourceModel>(ResourceCollectionName);
+      UserCollection = _db.GetCollection<UserModel>(UserCollectionName);
+   }
+
 }
---
+++ QuantumWorldAppLibrary/Data Access/IDbConnection.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Driver;
+
+namespace QuantumWorldAppLibrary.Data_Access;
+public interface IDbConnection
+{
+   IMongoCollection<BuildingModel> BuildingCollection { get; }
+   string BuildingCollectionName { get; }
+   MongoClient Client { get; }
+   string DbName { get; }
+   IMongoCollection<ResourceModel> ResourceCollection { get; }
+   string ResourceCollectionName { get; }
+   IMongoCollection<UserModel> UserCollection { get; }
+   string UserCollectionName { get; }
+}
