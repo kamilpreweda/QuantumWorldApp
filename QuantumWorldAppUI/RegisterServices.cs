6239029b21a0583d73a41961123ee2e347b7a5fb
Kamil Preweda <preweda.kamil@o2.pl>
2022-11-30 16:05:30 +0100

    Configured dependecy injection

--- QuantumWorldAppUI/RegisterServices.cs
+++ QuantumWorldAppUI/RegisterServices.cs
@@ -7,6 +7,11 @@ public static class RegisterServices
       builder.Services.AddRazorPages();
       builder.Services.AddServerSideBlazor();
       builder.Services.AddMemoryCache();
+      builder.Services.AddScoped<IDbConnection, DbConnection>();
+      builder.Services.AddScoped<IBuildingData, MongoBuildingData>();
+      builder.Services.AddScoped<IResourceData, MongoResourceData>();
+      builder.Services.AddScoped<IUserData, MongoUserData>();
+
    }
 }
 
