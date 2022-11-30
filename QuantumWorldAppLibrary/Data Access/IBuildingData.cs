f4400db170b0a0b433201b584a254e9d1e873212
Kamil Preweda <preweda.kamil@o2.pl>
2022-11-30 16:03:26 +0100

    Created Mongo Building Data and extracted an interface

---
+++ QuantumWorldAppLibrary/Data Access/IBuildingData.cs
@@ -0,0 +1,7 @@
+﻿namespace QuantumWorldAppLibrary.Data_Access;
+
+public interface IBuildingData
+{
+   Task CreateBuilding(BuildingModel building);
+   Task<List<BuildingModel>> GetAllBuildings();
+}
---
+++ QuantumWorldAppLibrary/Data Access/MongoBuildingData.cs
@@ -0,0 +1,34 @@
+﻿
+using Microsoft.Extensions.Caching.Memory;
+
+namespace QuantumWorldAppLibrary.Data_Access;
+public class MongoBuildingData : IBuildingData
+{
+   private readonly IMongoCollection<BuildingModel> _buildings;
+   private readonly IMemoryCache _cache;
+   private const string cacheName = "BuildingData";
+
+
+   public MongoBuildingData(IDbConnection db, IMemoryCache cache)
+   {
+      _cache = cache;
+      _buildings = db.BuildingCollection;
+   }
+   public async Task<List<BuildingModel>> GetAllBuildings()
+   {
+      var output = _cache.Get<List<BuildingModel>>(cacheName);
+      if (output is null)
+      {
+         var results = await _buildings.FindAsync(_ => true);
+         output = results.ToList();
+
+         _cache.Set(cacheName, output, TimeSpan.FromDays(1));
+      }
+      return output;
+   }
+   public Task CreateBuilding(BuildingModel building)
+   {
+      return _buildings.InsertOneAsync(building);
+   }
+}
+
